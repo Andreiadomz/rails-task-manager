485f735681a6d96ab19cc795b0449da5a6181a2f
Andreia Domingues <andreianetface@gmail.com>
2018-05-22 15:28:42 +0100

    all working except formatting in list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
 
-  get 'task/:id', to: 'tasks#show', as: :task
+  get 'tasks/new', to: 'tasks#new', as: :new_task # U
+  post 'tasks', to: 'tasks#create' # C
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # U
+  patch 'tasks/:id', to: 'tasks#update' # U
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task # D
+
 
 end
